502941607620cb9c3ea48382b6fed90528f4d5f0
Mateus <1mateus1felipe11@gmail.com>
2024-11-07 16:48:08 -0300

    Adicionar arquivos de projeto.

---
+++ Hello World/Names/GetName.cs
@@ -0,0 +1,9 @@
+﻿namespace Hello_World.Names;
+internal class GetName
+{
+    public string getUserName()
+    {
+        var userName = Console.ReadLine();
+        return userName;
+    }
+}
---
+++ Hello World/Program.cs
@@ -0,0 +1,21 @@
+﻿using Hello_World.Names;
+
+namespace HelloWord;
+class Program //Default: internal
+{
+    static void Main() 
+    {
+        GetName name = new GetName();
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Me livrei da maldição :)");
+        Console.Write("Digite seu nome: ");
+        var userName = name.getUserName();
+        Console.Write("Seu nome é: ");
+        ShowName(userName);
+    }
+
+    private static void ShowName(string name) //Default: Private
+    {
+        Console.WriteLine(name);
+    }
+}
